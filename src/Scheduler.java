c603abd51ee3e726637e6c4bacefdf140f9d8eae
fatema09 <123894379+fatema09@users.noreply.github.com>
2025-05-09 16:46:11 +0300

    added main class

---
+++ src/Scheduler.java
@@ -0,0 +1,13 @@
+// Fatema Redha Hamza           202102467
+// Sara Mansoor Mahmood         202109880
+// Fatima Mohamed Hasan Abdulla 202206839
+// Eman
+// Section: 2
+
+package src;
+
+public class Scheduler {
+    public static void main(String[] args) {
+
+    }
+}
